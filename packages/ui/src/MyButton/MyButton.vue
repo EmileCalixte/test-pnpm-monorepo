c849e95e9b2e308d36b1157cc1638afac8d93a81
Emile Calixte <calixte.emile@gmail.com>
2024-06-26 22:54:44 +0200

    first commit

---
+++ packages/ui/src/MyButton/MyButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="bg-brand-main px-3 py-1 shadow-sm hover:bg-opacity-90 hover:shadow-lg">
+    <slot />
+  </button>
+</template>
---
+++ packages/ui/src/main.ts
@@ -0,0 +1,3 @@
+import './style.css'
+
+export { default as MyButton } from './MyButton/MyButton.vue'
